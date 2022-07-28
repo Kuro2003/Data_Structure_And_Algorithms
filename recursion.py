7b3ae250586cd5526311514a0029f98407f28cd9
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-28 10:02:36 +0700

    add lesson 2

--- recursion.py
+++ recursion.py
@@ -1,8 +1,22 @@
-""" Lesson 1: calculate n! """
 def cal_les_1(n):
     if n == 0:
         return 1
     return n * cal_les_1(n-1)
 
-n = int(input("Enter n = "))
-print(cal_les_1(n))
+def cal_les_2(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return cal_les_2(n-1) + cal_les_2(n-2)
+
+def main():
+    """ Lesson 1: calculate n! """
+    # n = int(input("Enter n = "))
+    # print(cal_les_1(n))
+
+    """ Lesson 2: Fibo """
+    n = int(input("Enter n = "))
+    print(cal_les_2(n))
+
+main()
