69e2844cc38e5f188c387e99c3542c0fdf85cfee
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-28 18:38:55 +0700

    binary_search

--- recursion.py
+++ recursion.py
@@ -1,3 +1,6 @@
+from sklearn.compose import TransformedTargetRegressor
+
+
 def show_menu():
     print("\n                        Menu                        ")
     print("-----------------------------------------------------")
@@ -107,6 +110,31 @@ def les_6():
     major_length = int(input("Enter major length: "))
     draw_ruler(num_inches,major_length)
 
+def binary_search(data,target,low,high):
+    """ Return True if target is found in indicated portion of a Python list.
+    The search only considers the portion from data[low] to data[high] inclusive.
+    """
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            # recur on the portion left of the middle
+            return binary_search(data,target,low,mid-1)
+        else:
+            # recur on the portion right of the middle
+            return binary_search(data,target,mid+1,high)
+
+def les_7():
+    data = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,37]
+    target = int(input("Enter number you find: "))
+    if binary_search(data,target,0,len(data)-1) == True:
+        print("find " + str(target) + " in data")
+    else:
+        print("find " + str(target) + " not in data")
+
 def main():
     while True:
         show_menu()
@@ -123,6 +151,8 @@ def main():
             les_5()
         elif user_input == 6:
             les_6()
+        elif user_input == 7:
+            les_7()
         elif user_input == 0:
             break
 
--- test.py
+++ test.py
@@ -1,20 +1,7 @@
-def binary_search(data,target,low,high):
-    """ Return True if target is found in indicated portion of a Python list.
-    The search only considers the portion from data[low] to data[high] inclusive.
-    """
-    if low > high:
-        return False
+def fibo(n):
+    if n <= 1:
+        return (n,0)
     else:
-        mid = (low + high) // 2
-        if target == data[mid]:
-            return True
-        elif target < data[mid]:
-            # recur on the portion left of the middle
-            return binary_search(data,target,low,mid-1)
-        else:
-            # recur on the portion right of the middle
-            return binary_search(data,target,mid+1,high)
-
-data = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,37]
-print(binary_search(data,55,0,len(data)-1))
-
+        (a,b) = fibo(n-1)
+        return (a+b,a)
+print(fibo(3))
