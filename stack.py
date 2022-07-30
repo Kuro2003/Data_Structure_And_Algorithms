5d57ccce89db4ba84192311778e083f0f9d65b4e
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-30 22:24:12 +0700

    update code

--- stack.py
+++ stack.py
@@ -1,22 +1,30 @@
 class Empty(Exception):
     pass
 class ArrayStack:
-    def __int__(self):
+    def __init__(self):
         """ Create an empty stack."""
         self._data = []
 
     def __len__(self):
         """ Return the number of elements in the stack."""
         return len(self._data)
-    
+
     def is_empty(self):
         """ Return True if the stack is empty """
         return len(self._data) == 0
-    
+
     def push(self,e):
         """ Add element e to the top of the stack"""
         self._data.append(e)
     
+    def pop(self):
+        """ Remove and return the element from the top of the stack
+            Raise Empty exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        return self._data.pop()
+
     def top(self):
         """ Return (but do not remove) the element at the top of the stack
             Raise Empty exception if the stack is empty
@@ -24,14 +32,6 @@ class ArrayStack:
         if self.is_empty():
             raise Empty("Stack is empty")
         return self._data[-1]
-    
-    def pop(self):
-        """ Remove and return the element from the top of the stack
-            Raise Empty exception if the stack is empty
-        """
-        if self.is_empty():
-            raise Empty('Stack is empty')
-        return self._data.pop()
 
 def checkOp(op):
     if op == '+' or op == '-' or op == '*' or op == '/':
