8fc7c957565970716cb2e63b72bee4cbb2c7a633
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-30 21:47:28 +0700

    update code

--- stack.py
+++ stack.py
@@ -1,3 +1,37 @@
+class Empty(Exception):
+    pass
+class ArrayStack:
+    def __int__(self):
+        """ Create an empty stack."""
+        self._data = []
+
+    def __len__(self):
+        """ Return the number of elements in the stack."""
+        return len(self._data)
+    
+    def is_empty(self):
+        """ Return True if the stack is empty """
+        return len(self._data) == 0
+    
+    def push(self,e):
+        """ Add element e to the top of the stack"""
+        self._data.append(e)
+    
+    def top(self):
+        """ Return (but do not remove) the element at the top of the stack
+            Raise Empty exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        return self._data[-1]
+    
+    def pop(self):
+        """ Remove and return the element from the top of the stack
+            Raise Empty exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
 
 def checkOp(op):
     if op == '+' or op == '-' or op == '*' or op == '/':
@@ -7,7 +41,7 @@ def checkOp(op):
 def cal(s):
     s = s.split(' ')
     i = 0
-    st = []
+    st = ArrayStack()
     t = 0
     while(len(s) != i):
         if checkOp(s[i]):
@@ -22,11 +56,12 @@ def cal(s):
             else:
                 t = a / b
 
-            st.append(t)
+            st.push(t)
         else:
-            st.append(s[i])
+            st.push(s[i])
         i += 1
     return t
+
 def main():
     s = '5 10 + 2 * 3 /'
     res = cal(s)
