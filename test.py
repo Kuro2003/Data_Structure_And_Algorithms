dffa445417a360b0d10a77a6032243fe0ba44229
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-31 15:54:45 +0700

    exercise super prime

--- test.py
+++ test.py
@@ -1,17 +1,31 @@
-stack=[]
-s=input()
-s+="#"
-answer=""
-# for i in range(len(s)):
-#     if not stack or stack[-1] == s[i]:
-#         stack.append(s[i])
-#     else:
-#         answer += stack[-1]
-#         count=0
-#         while stack:
-#             stack.pop()
-#             count += 1
-#         answer += str(count)
-#         stack.append(s[i])
-# print(answer)
-print(not stack)
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+def is_spr_prime(n):
+    while n != 0 :
+        if is_prime(n):
+            pass
+        else:
+            return False
+        n = n // 10
+    return True
+
+queue = []
+temp = []
+n = int(input())
+for i in range(2,n+1):
+    if is_prime(i):
+        temp.append(i)
+print(temp)
+for i in temp:
+    if is_spr_prime(i):
+        queue.append(i)
+print(queue)
+for i in range(len(queue)):
+    print(queue[i],end = ' ')
+print(is_spr_prime(3))
