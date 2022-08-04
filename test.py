b5b16cd6e853450600c42bb2d69e2d8750e4e7e5
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-08-04 21:22:47 +0700

    test code

--- test.py
+++ test.py
@@ -1,56 +1,26 @@
-#!/bin/python3
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
 
-import math
-import os
-import random
-import re
-import sys
+def is_spr_prime(n):
+    while n != 0 :
+        if is_prime(n):
+            pass
+        else:
+            return False
+        n = n // 10
+    return True
 
-#
-# Complete the 'countApplesAndOranges' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER s
-#  2. INTEGER t
-#  3. INTEGER a
-#  4. INTEGER b
-#  5. INTEGER_ARRAY apples
-#  6. INTEGER_ARRAY oranges
-#
+queue = []
+n = int(input())
+for i in range(2,n+1):
+    if is_prime(i):
+        if is_spr_prime(i):
+            queue.append(i)
+            print(queue[0],end=' ')
+            queue.pop()
 
-def countApplesAndOranges(s, t, a, b, apples, oranges):
-    # Write your code here
-    count = 0
-    for i in apples:
-        if i + a >= s and i + a <= t:
-            count += 1
-    print(count)
-    count = 0
-    for i in range(len(oranges)):
-        if oranges[i] + b >= s and oranges[i] + b <= t:
-            count += 1
-    print(count)
-if __name__ == '__main__':
-    first_multiple_input = input().rstrip().split()
-
-    s = int(first_multiple_input[0])
-
-    t = int(first_multiple_input[1])
-
-    second_multiple_input = input().rstrip().split()
-
-    a = int(second_multiple_input[0])
-
-    b = int(second_multiple_input[1])
-
-    third_multiple_input = input().rstrip().split()
-
-    m = int(third_multiple_input[0])
-
-    n = int(third_multiple_input[1])
-
-    apples = list(map(int, input().rstrip().split()))
-
-    oranges = list(map(int, input().rstrip().split()))
-
-    countApplesAndOranges(s, t, a, b, apples, oranges)
