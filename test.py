66bcb57c60eb0042c654d82696358cd12b404edc
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-08-11 23:40:03 +0700

    test case exercise

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-d = 0
-i = 20
-j = 23
-k = 6
-for k in range(i,j+1):
-    if abs(l - int(str(l)[::-1])) % k == 0:
-        d += 1
-print(d)
+def aa():
+    global a 
+    a = 10
+    def bb():
+        global a 
+        a = 30
+a = 12
+aa()
+print(a)
