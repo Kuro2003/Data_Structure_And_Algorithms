99a07cf0b5ee6c59a734d0fd14c4176ea0b50f02
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-08-22 10:33:11 +0700

    find node 2 one children and node leaf

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,6 +1,4 @@
-from operator import le
-from os import cpu_count
-from time import sleep
+from platform import node
 
 
 class Node:
@@ -10,6 +8,7 @@ class Node:
 		self.right_child = None
 
 class Binary_Search_Tree:
+	# Initial
 	def __init__(self):
 		# Initial root
 		self.root = None
@@ -76,16 +75,64 @@ class Binary_Search_Tree:
 		elif value > cur_node.value and cur_node.right_child != None:
 			return self._find(cur_node.right_child,value)
 		return False
+	
+	# Find node 2 children
+	def node_2_children(self):
+		if self.root != None:
+			self._node_two_children(self.root)
+		else:
+			print("Tree is empty!!!")
+	
+	def _node_two_children(self,cur_node):
+		if cur_node != None:
+			if cur_node.left_child != None and cur_node.right_child != None:
+				print(cur_node.value,end= ' ')
+			self._node_two_children(cur_node.left_child)
+			self._node_two_children(cur_node.right_child)
+
+	# Find node one children
+	def node_one_children(self):
+		if self.root != None:
+			self._node_one_children(self.root)
+		else:
+			print("Tree is empty!!!")
+
+	def _node_one_children(self,cur_node):
+		if cur_node != None:
+			if (cur_node.left_child != None and cur_node.right_child == None) or (cur_node.left_child == None and cur_node.right_child != None):
+				print(cur_node.value,end = ' ')
+			self._node_one_children(cur_node.left_child)
+			self._node_one_children(cur_node.right_child)
+	
+	# Find node leaf
+	def node_leaf(self):
+		if self.root != None:
+			self._node_leaf(self.root)
+		else:
+			print("Tree is empty!!!")
+	
+	def _node_leaf(self,cur_node):
+		if cur_node != None:
+			if (cur_node.left_child == None and cur_node.right_child == None):
+				print(cur_node.value,end = ' ')
+			self._node_leaf(cur_node.left_child)
+			self._node_leaf(cur_node.right_child)
 if __name__ == '__main__':
 	tree = Binary_Search_Tree()
-	tree.insert(7)
-	tree.insert(6)
-	tree.insert(8)
-	tree.insert(10)
-	tree.insert(3)
 	tree.insert(5)
-	tree.insert(4)
+	tree.insert(1)
+	tree.insert(-2)
+	tree.insert(0)
+	tree.insert(8)
+	tree.insert(7)
+	tree.insert(9)
 	tree.print_tree()
 	print("\nThe height is " + str(tree.height()))
 	print(tree.find(3))
 	print(tree.find(12))
+	print("Node 2 children is: ",end='')
+	tree.node_2_children()
+	print("\nNode 1 children is: ",end='')
+	tree.node_one_children()
+	print("\nNode leaf is: ",end='')
+	tree.node_leaf()
