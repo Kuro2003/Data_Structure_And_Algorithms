9b0fa9b2833157253d60f5c4716913c03033398e
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-09-07 16:56:37 +0700

    create folder and selection sort

---
+++ Sorting/selectionsort.py
@@ -0,0 +1,11 @@
+a = [1,2,3,5,6,0,12,42,23,65]
+def selectionsort(a,n):
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1,n):
+            if a[min_idx] > a[j]:
+                min_idx = j
+        a[i], a[min_idx] = a[min_idx],a[i]
+print(a)
+selectionsort(a,len(a))
+print(a)
