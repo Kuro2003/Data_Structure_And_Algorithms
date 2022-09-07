0d660ca5a4d9a7e90b2915242d1e218e444761d8
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-09-07 16:41:00 +0700

    bubble sort

--- test.py
+++ test.py
@@ -1,52 +1,9 @@
-# #!/bin/python3
-
-# import math
-# import os
-# import random
-# import re
-# import sys
-
-# #
-# # Complete the 'circularArrayRotation' function below.
-# #
-# # The function is expected to return an INTEGER_ARRAY.
-# # The function accepts following parameters:
-# #  1. INTEGER_ARRAY a
-# #  2. INTEGER k
-# #  3. INTEGER_ARRAY queries
-# #
-
-# def circularArrayRotation(a, k, queries):
-#     # Write your code here
-#     for _ in range(k-1):
-#         for i in range(len(a),0):
-#             a[i] = a[i-1]
-#         a[0] = a[len(a)-1]
-#         a.pop()
-#     b = []
-#     for i in range(queries):
-#         b.append(a[queries[i]])
-#     return b
-        
-
-# if __name__ == '__main__':
-#     first_multiple_input = input().rstrip().split()
-
-#     n = int(first_multiple_input[0])
-
-#     k = int(first_multiple_input[1])
-
-#     q = int(first_multiple_input[2])
-
-#     a = list(map(int, input().rstrip().split()))
-
-#     queries = []
-
-#     for _ in range(q):
-#         queries_item = int(input().strip())
-#         queries.append(queries_item)
-
-#     result = circularArrayRotation(a, k, queries)
-#     print(result)
-for i in range(10,0,-1):
-    print(i)
+a = [1,2,3,5,6,0,12,42,23,65]
+def bubblesort(a,n):
+    for i in range(n):
+        for j in range(n-i-1):
+            if a[j] > a[j+1]:
+                a[j],a[j+1] = a[j+1] ,a[j]
+print(a)
+bubblesort(a,len(a))
+print(a)
