8c17b5d6857758b10454dd2fb9cee8bf8d2ba4a8
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-09-17 08:23:37 +0700

    review sorting

--- Sorting/quick sort.py
+++ Sorting/quick sort.py
@@ -1,18 +1,20 @@
+""" Method 1: Implementation of QuickSort using the end element as a pivot:"""
+# Function to find partition position
 def partition(arr,low,high):
     pivot = arr[high]
     i = low - 1
     for j in range(low,high):
-        if arr[j] <= pivot:
+        if arr[j] < pivot:
             i += 1
             arr[i],arr[j] = arr[j],arr[i]
-    arr[i+1],arr[high] = arr[high],arr[i+1]
-
-    return i+1
+    arr[i+1],pivot = pivot,arr[i+1]
+    return i +1
+# Function to ferform quicksort
 def quicksort(arr,low,high):
-    if low < high:
+    if low < high :
         pi = partition(arr,low,high)
         quicksort(arr,low,pi-1)
-        quicksort(arr,pi+1,high)
+        quicksort(arr,pi+1, high)
 
 arr = [1,2,3,5,6,0,12,42,23,65]
 print("Mang khoi tao:\n",arr)
