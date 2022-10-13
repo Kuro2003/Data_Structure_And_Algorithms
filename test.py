30407f75cafc43263d495b96df60e39f9173a92c
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-10-13 09:18:04 +0700

    do exercise

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
 # Initialize a string you want to remove leading zeros
-str_left = "000124421"
+str_left = "000Learn Share IT00000"
 
-# Remove leading zeros
+# Remove leading and trailing zeros
 new_str_1 = str_left.lstrip("0")
+new_str_2 = new_str_1.rstrip("0")
 
 # Print new string
-print("New string remove leading zeros:",new_str_1)
-
-# Initialize a string you want to remove trailing zeros
-str_left = "959500000"
-
-# Remove leading zeros
-new_str_2 = str_left.rstrip("0")
-
-# Print new string
-print("New string remove trailing zeros:",new_str_2)
+print("New string remove leading and trailing zeros:",new_str_2)
