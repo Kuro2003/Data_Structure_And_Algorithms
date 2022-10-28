e06d1e3990921b8910a2982b6796a9916a986765
Kuro2k3 <cuonghacknao2003@gmail.com>
2022-10-28 21:23:34 +0700

    remove\r\n method 2

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # remove \r\n from a string of strings
 str = "\r Learn Share IT \r\n"
 
-res = str.strip()
+res = str.replace("\n","").replace("\r","")
 print(res)  # Learn Share IT
 
 # -------------------------------------
@@ -10,6 +10,6 @@ print(res)  # Learn Share IT
 # Initial a list contain strings
 a = ['\r line 1\r\n', '\n line 2\r\n', '\r\n line 3\r\n']
 
-res1 = [i.strip() for i in a ]
+res1 = [i.replace("\n","").replace("\r","") for i in a ]
 print(res1) # ['line 1', 'line 2', 'line 3']
 
