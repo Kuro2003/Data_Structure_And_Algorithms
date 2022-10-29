7aaf4038905e73166ec0eba6991cf1bcc92730a6
Kuro2k3 <cuonghacknao2003@gmail.com>
2022-10-29 09:36:12 +0700

    fix thread

--- test.py
+++ test.py
@@ -1,15 +1,24 @@
-# remove \r\n from a string of strings
-str = "\r Learn Share IT \r\n"
+import thread
 
-res = str.replace("\n","").replace("\r","")
-print(res)  # Learn Share IT
+def print_rectangular(num1,num2,num3):
+    print("Cube: {}" .format(num1 * num2 * num3))
 
-# -------------------------------------
+def print_rectangle(num1,num2):
+    # function to print square of given num
+    print("Square: {}" .format(num1 * num2))
 
-# remove \r\n from a list of  strings
-# Initial a list contain strings
-a = ['\r line 1\r\n', '\n line 2\r\n', '\r\n line 3\r\n']
+if __name__ =="__main__":
+    # creating thread
+    t1 = threading.Thread(target=print_rectangle, args=(10,20))
+    t2 = threading.Thread(target=print_rectangular, args=(10,20,30))
 
-res1 = [i.replace("\n","").replace("\r","") for i in a ]
-print(res1) # ['line 1', 'line 2', 'line 3']
+    # starting thread
+    t1.start()
+    t2.start()
 
+    # wait thread is executed successful
+    t1.join()
+    t2.join()
+
+    # thread executed cuccessful
+    print("Done!") 
